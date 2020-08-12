28ea06be0dc7f944b196ba837ea7f48bbf748984
drewparker <drewjparker91@gmail.com>
2020-08-12 10:06:04 -0700

    Add add javasctript
    Co-authored-by: Mike Manchee <mikemanchee@gmail.com>
    Co-authored-by: Drew Macauley <macauleyandy03@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
-$(document).ready(function(event) {
-  $("#")
-})
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    const person1Input = $("input#person1").val();
+    const dogNameInput = $("input#dogName").val();
+    const monsterInput = $("input#monster").val();
+    const exclamationInput = $("input#exclamation").val();
+    const verbInput = $("input#verb").val();
+    const nounInput = $("input#noun").val();
+
+    $(".person1").text(person1Input);
+    $(".dogName").text(dogNameInput);
+    $(".monster").text(monsterInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nounInput);
+
+    $("#story").show();
+
+    event.preventDefault();
+  });
+});
